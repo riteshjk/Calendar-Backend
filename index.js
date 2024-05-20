a26a761f55a34fba73b96ce393ed1acee7ad2442
riteshjk <96072567+riteshjk@users.noreply.github.com>
2024-05-20 17:43:42 +0530

    more chnages

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors({origin:"*"}));
+app.use(cors());
 app.use(cookieParser());
 app.use("/auth",userRoute)
 app.use("/auth",eventRouter)
